41fc9e9779f67828aa0023373a2634b75a093eea
happytoolkits <124449822+happytoolkits@users.noreply.github.com>
2023-02-04 23:05:05 +0700

    Update streamlit_app.py
    add my_fruit_list

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page.
 
-my_fruit_list = pandas.readcsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit macros.txt")
+my_fruit_list = pandas.readcsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 #display the table on the page
-streamlit.dataframe (my fruit list)
+streamlit.dataframe (my_fruit_list)
