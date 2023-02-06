8a9833e7d882cce551df8f45ed15a092ec4159c1
happytoolkits <124449822+happytoolkits@users.noreply.github.com>
2023-02-06 13:19:47 +0700

    Update streamlit_app.py
    Add breakfast menu header and 3 items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,8 @@
 import streamlit
-import pandas
 
+streamlit.title('My Parents New Healthy Diner')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
-
-# Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-
-# Display the table on the page.
-
-my_fruit_list = pandas.readcsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
-#display the table on the page
-streamlit.dataframe (my_fruit_list)
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
