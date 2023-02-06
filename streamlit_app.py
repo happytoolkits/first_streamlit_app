8e3de7728c49ea015dc8fe1bb1daf4656ae917ba
happytoolkits <124449822+happytoolkits@users.noreply.github.com>
2023-02-06 13:26:19 +0700

    Update streamlit_app.py
    fixed typo

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,6 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 (3 Kale, Spinach & Rocket Smoothie'
-streamlit.text('🐔 4 Hard-Boiled Free-Range Egg'
+streamlit.text('🥗 (3 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 4 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
