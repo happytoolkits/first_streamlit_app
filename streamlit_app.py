a2eb5f2ed780201c268fce0bf6bd7bfeb1bf7e8b
happytoolkits <124449822+happytoolkits@users.noreply.github.com>
2023-02-07 18:21:52 +0700

    Update streamlit_app.py
    Changing headers

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('Breakfast Favorites')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 (3 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 4 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
